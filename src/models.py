b4574e219f175c5e424cc1576db399a88951489f
Тутуев Максим Сергеевич <TutuevMS@MILKOM-KOMOS.RU>
2024-09-24 20:47:38 +0400

    create weather_data model

---
+++ src/models.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from uuid import UUID
+
+from sqlalchemy import DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import Base
+
+
+class WeatherData(Base):
+    __tablename__ = "weather_data"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    time: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, default=datetime.now
+    )
+    temperature: Mapped[float] = mapped_column(nullable=False)
+    wind_direction: Mapped[int] = mapped_column(nullable=False)
+    wind_speed: Mapped[float] = mapped_column(nullable=False)
